c4158796000e802a0a916c1aa3b8062a782e0d45
Sehzan <sehzanhitech@gmail.com>
2026-06-13 01:14:03 +0530

    Add QuickSort

--- Test.java
+++ Test.java
@@ -1,8 +1,27 @@
+
+import java.util.Scanner;
+
 public class Test
 {
-
     public static void main(String[] args)
     {
-        
+        try(Scanner sc = new Scanner(System.in))
+        {
+            int n, arr[];
+            System.out.print("Enter no. of elements: ");
+            n = sc.nextInt();
+            arr = new int[n];
+            System.out.println("Enter elements: ");
+            for(int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+                /////////
+            System.out.println("Sorted: ");
+            for(int i = 0; i < n; i++)
+                System.out.print(arr[i]+" ");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error occured: "+e);
+        }  
     }   
 }
--- com/hitech/sorting/QuickSort.java
+++ com/hitech/sorting/QuickSort.java
@@ -1,9 +1,56 @@
 package com.hitech.sorting;
 
+import java.util.Scanner;
 
 public class QuickSort
 {
-    public static void main(String[] args) {
-        
+
+    public static void quickSort(int[] arr, int low, int high)
+    {
+        if(low < high)
+        {
+            int pivot = arr[low];
+            int i = low;
+            int j = high;
+            while(i < j)
+            {
+                while(arr[i] <= pivot && i < high)
+                    i++;
+                while(arr[j] > pivot && j > low)
+                    j--;
+                if(i < j)
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            arr[low] = arr[j];
+            arr[j] = pivot;
+
+            quickSort(arr, low, j-1);
+            quickSort(arr, j+1, high);
+        }
     }
+    public static void main(String[] args)
+    {
+        try(Scanner sc = new Scanner(System.in))
+        {
+            int n, arr[];
+            System.out.print("Enter no. of elements: ");
+            n = sc.nextInt();
+            arr = new int[n];
+            System.out.println("Enter elements: ");
+            for(int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+            quickSort(arr, 0, n-1);
+            System.out.println("Sorted: ");
+            for(int i = 0; i < n; i++)
+                System.out.print(arr[i]+" ");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error occured: "+e);
+        }  
+    }   
 }
