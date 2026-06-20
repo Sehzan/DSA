488f6426ac6673df05159be59d2f2255c52906ec
Sehzan <sehzanhitech@gmail.com>
2026-06-20 23:57:46 +0530

    Longest Sequence

---
+++ com/hitech/leetcode/Leetcode128LongestConsecutiveSequence.java
@@ -0,0 +1,87 @@
+package com.hitech.java;
+
+import java.util.HashSet;
+
+public class Leetcode128LongestConsecutiveSequence
+{
+    public int longestConsecutive(int[] arr) 
+    {
+        int n = arr.length;
+        if(n == 0) return 0;
+        int largest = 1, count; 
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < n; i++)
+            set.add(arr[i]);
+        for(Integer num : set)
+        {
+            if(!set.contains(num-1))
+            {
+                count = 1;
+                for(int i = num + 1; set.contains(i); i++)
+                { 
+                    count++;
+                }
+                largest = Math.max(largest, count);
+            }
+
+        }
+        return largest;
+    }
+         static 
+    {
+        for(int i=0;i<500;i++)
+            longestConsecutive(new int[]{});
+    }
+    
+//     public static int longestConsecutive(int[] nums) {
+//         if (nums.length == 0) {
+//             return 0;
+//         }
+//         int min = nums[0];
+//         int max = nums[0];
+
+//         for (int num : nums){
+//             max = Math.max(num, max);
+//             min = Math.min(num, min);
+//         }
+
+//         if (max < 100000) {
+//             boolean [] apariciones = new boolean [max - min + 1];
+//             for (int num : nums) {
+//                 apariciones[num - min] = true;
+//             }
+
+//             int maxTotal = 0;
+//             int maxLocal = 0;
+//             for (boolean a : apariciones) {
+//                 if (a) {
+//                     maxLocal++;
+//                 } else {
+//                     maxTotal = Math.max(maxTotal, maxLocal);
+//                     maxLocal = 0;
+//                 }
+//             }
+//             maxTotal = Math.max(maxTotal, maxLocal);
+//             return maxTotal;
+//         } else {
+//             Set<Integer> uniNums = new HashSet();
+//             for( int num : nums ){
+//                 uniNums.add(num);
+//             }
+
+//             int maxB = 0;
+//             for ( int num : uniNums ){
+//                 if (!uniNums.contains(num - 1)){
+//                     int endSeq = num;
+//                     while (uniNums.contains(endSeq+1)){
+//                         endSeq++;
+//                     }
+//                     maxB = Math.max(maxB, endSeq - num + 1);
+//                 }
+//             }
+//             return maxB;
+//         }
+//     }
+// }
+}
